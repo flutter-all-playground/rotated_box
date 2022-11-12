cfc21891583912ebdff2a9362b85ff26b2607b0a
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-11-12 18:29:42 -0300

    Add Rotated Box sample - Rotated the child widget

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rotated_box
+
+<div align='center'>
+  <img src='https://github.com/phferreira/assets/blob/master/images/rotated_box.png'/>
+</div>
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rotated Box',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 74),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rotated Box'),
+      ),
+      body: const Center(
+        child: RotatedBox(
+          quarterTurns: 1,
+          child: FlutterLogo(size: 100),
+        ),
+      ),
+    );
+  }
+}
